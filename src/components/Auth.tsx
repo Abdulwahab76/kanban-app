ffa6bce22e6b6b349664d4922664b7f18f6d6dc6
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2026-01-29 20:09:50 +0500

    fix: origin url

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -77,8 +77,9 @@ export default function AuthComponent() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: window.location.origin
+                    redirectTo: `${window.location.origin}/auth/callback`
                 }
+
             });
 
             if (error) throw error;
